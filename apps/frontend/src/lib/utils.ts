d2fe00682081b297d7f8c146c4525ea7c30a1e83
Branko Ottavianelli <branko-96@hotmail.com>
2026-05-30 20:57:10 -0300

    feat: add remainingSlots counter helper to lib/utils

---
+++ apps/frontend/src/lib/utils.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns how many slots are left given the number used and the total available.
+ */
+export function remainingSlots(used: number, total: number): number {
+  return total - used + 1;
+}
